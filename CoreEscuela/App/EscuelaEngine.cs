52056a151197cddd93bbeaa844a290d3e66d6581
Hector Hugo Moreira <hector.moreirac@gmail.com>
2021-10-26 19:00:29 -0500

    Regiones

--- CoreEscuela/App/EscuelaEngine.cs
+++ CoreEscuela/App/EscuelaEngine.cs
@@ -8,49 +8,28 @@ namespace CoreEscuela.App
     public sealed class EscuelaEngine
     {
         public Escuela Escuela { get; set; }
+
         public EscuelaEngine()
         {
-
         }
 
         public void Inicializar()
         {
-            Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Secundaria, pais: "Colombia", ciudad: "Bogotá");
+            Escuela =
+                new Escuela("Platzi Academy",
+                    2012,
+                    TiposEscuela.Secundaria,
+                    pais: "Colombia",
+                    ciudad: "Bogotá");
             CargarCursos();
             CargarAsignaturas();
             CargarEvaluaciones();
         }
 
-        private void CargarEvaluaciones()
-        {
-
-            foreach (var curso in Escuela.Cursos)
-            {
-                foreach (var asignatura in curso.Asignaturas)
-                {
-                    foreach (var alumno in curso.Alumnos)
-                    {
-                        var rnd = new Random(System.Environment.TickCount);
-                        for (int i = 0; i < 5; i++)
-                        {
-                            var evaluacion = new Evaluacion
-                            {
-                                Asignatura = asignatura,
-                                Nombre = $"{asignatura.Nombre} Ev#{i + 1}",
-                                Nota = (float)(5 * rnd.NextDouble()),
-                                Alumno = alumno
-                            };
-                            alumno.Evaluaciones.Add(evaluacion);
-                        }
-                    }
-                }
-            }
-        }
-
         public List<ObjetoEscuelaBase> GetObjetosEscuela()
         {
             var listObj = new List<ObjetoEscuelaBase>();
-            listObj.Add(Escuela);
+            listObj.Add (Escuela);
             listObj.AddRange(Escuela.Cursos);
 
             foreach (var curso in Escuela.Cursos)
@@ -66,43 +45,110 @@ namespace CoreEscuela.App
             return listObj;
         }
 
+        #region Metodos de carga
+
+        private void CargarEvaluaciones()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                foreach (var asignatura in curso.Asignaturas)
+                {
+                    foreach (var alumno in curso.Alumnos)
+                    {
+                        var rnd = new Random(System.Environment.TickCount);
+                        for (int i = 0; i < 5; i++)
+                        {
+                            var evaluacion =
+                                new Evaluacion {
+                                    Asignatura = asignatura,
+                                    Nombre = $"{asignatura.Nombre} Ev#{i + 1}",
+                                    Nota = (float)(5 * rnd.NextDouble()),
+                                    Alumno = alumno
+                                };
+                            alumno.Evaluaciones.Add (evaluacion);
+                        }
+                    }
+                }
+            }
+        }
+
         private void CargarAsignaturas()
         {
             foreach (var curso in Escuela.Cursos)
             {
-                List<Asignatura> listaAsignaturas = new List<Asignatura>(){
-                    new Asignatura{Nombre="Matemáticas"},
-                    new Asignatura{Nombre="Educación Física"},
-                    new Asignatura{Nombre="Castellano"},
-                    new Asignatura{Nombre="Ciencias Naturales"}
-                };
+                List<Asignatura> listaAsignaturas =
+                    new List<Asignatura>()
+                    {
+                        new Asignatura { Nombre = "Matemáticas" },
+                        new Asignatura { Nombre = "Educación Física" },
+                        new Asignatura { Nombre = "Castellano" },
+                        new Asignatura { Nombre = "Ciencias Naturales" }
+                    };
                 curso.Asignaturas = listaAsignaturas;
             }
         }
 
         private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
         {
-            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
-            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
-            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+            string[] nombre1 =
+            {
+                "Alba",
+                "Felipa",
+                "Eusebio",
+                "Farid",
+                "Donald",
+                "Alvaro",
+                "Nicolás"
+            };
+            string[] apellido1 =
+            {
+                "Ruiz",
+                "Sarmiento",
+                "Uribe",
+                "Maduro",
+                "Trump",
+                "Toledo",
+                "Herrera"
+            };
+            string[] nombre2 =
+            {
+                "Freddy",
+                "Anabel",
+                "Rick",
+                "Murty",
+                "Silvana",
+                "Diomedes",
+                "Nicomedes",
+                "Teodoro"
+            };
 
-            var listaAlumnos = from n1 in nombre1
-                               from n2 in nombre2
-                               from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+            var listaAlumnos =
+                from n1 in nombre1
+                from n2 in nombre2
+                from a1 in apellido1
+                select new Alumno { Nombre = $"{n1} {n2} {a1}" };
 
-            return listaAlumnos.OrderBy((al) => al.UniqueId).Take(cantidad).ToList();
+            return listaAlumnos
+                .OrderBy((al) => al.UniqueId)
+                .Take(cantidad)
+                .ToList();
         }
 
         private void CargarCursos()
         {
-            Escuela.Cursos = new List<Curso>(){
-                new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "201", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "301", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "401", Jornada = TiposJornada.Tarde },
-                new Curso(){ Nombre = "501", Jornada = TiposJornada.Tarde },
-            };
+            Escuela.Cursos =
+                new List<Curso>()
+                {
+                    new Curso()
+                    { Nombre = "101", Jornada = TiposJornada.Mañana },
+                    new Curso()
+                    { Nombre = "201", Jornada = TiposJornada.Mañana },
+                    new Curso()
+                    { Nombre = "301", Jornada = TiposJornada.Mañana },
+                    new Curso()
+                    { Nombre = "401", Jornada = TiposJornada.Tarde },
+                    new Curso() { Nombre = "501", Jornada = TiposJornada.Tarde }
+                };
             Random rnd = new Random();
             foreach (var curso in Escuela.Cursos)
             {
@@ -110,5 +156,6 @@ namespace CoreEscuela.App
                 curso.Alumnos = GenerarAlumnosAlAzar(cantidadRandom);
             }
         }
+        #endregion
     }
 }
